9ab1639a4f35a8e62def3d32c41771d8c9412628
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-09-23 23:13:43 +0530

    Update loginPage.dart

--- nextbus_passenger/lib/authPages/loginPage.dart
+++ nextbus_passenger/lib/authPages/loginPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nextbus_passenger/authPages/createAccountPage.dart';
 
 import '../colors.dart';
 import '../componants/button.dart';
@@ -14,7 +15,6 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
   final emailOrPhoneController = TextEditingController();
   final passwordController = TextEditingController();
 
@@ -32,41 +32,64 @@ class _LoginPageState extends State<LoginPage> {
               SizedBox(
                   width: 230,
                   height: 130,
-                  child: Image.asset('images/VerLogoBlue.png',)),
-
+                  child: Image.asset(
+                    'images/VerLogoBlue.png',
+                  )),
               SizedBox(
-                  width: 250,
-                  child: Image.asset('images/bro.png',)),
-
+                  width: 200,
+                  child: Image.asset(
+                    'images/bro.png',
+                  )),
               const SizedBox(
                 height: 20,
               ),
-
               MyTextField(
                 controller: emailOrPhoneController,
                 hintText: 'Email or Phone Number',
                 obscureText: false,
-                suffixIcon: Icon(Icons.person_outline_rounded,color: AppColor.iconColor,),),
-
+                suffixIcon: Icon(
+                  Icons.person_outline_rounded,
+                  color: AppColor.iconColor,
+                ),
+              ),
               MyTextField(
-                controller:passwordController,
+                controller: passwordController,
                 hintText: 'Enter Your Password',
                 obscureText: true,
-                suffixIcon: Icon(Icons.password_outlined,color: AppColor.iconColor,),),
-
+                suffixIcon: Icon(
+                  Icons.password_outlined,
+                  color: AppColor.iconColor,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Forgot Password?',
+                          style: TextStyle(color: Colors.redAccent,
+                            fontSize: 18,
+                          ),
+                        )),
+                  ],
+                ),
+              ),
               const SizedBox(
+                height: 15,
+              ),
+              MyButton(onTap: () {}, childText: 'Log In', width: 180),
+              SizedBox(
                 height: 20,
               ),
-
-              MyButton(onTap: (){}, childText: 'Log In', width: 180),
-
-              SizedBox(height: 20,),
-
-              Padding(padding: EdgeInsets.symmetric(horizontal: 25),
+              Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
                   child: Image.asset('images/Or.png')),
-
-              SizedBox(height: 15,),
-
+              SizedBox(
+                height: 15,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: const [
@@ -79,28 +102,34 @@ class _LoginPageState extends State<LoginPage> {
                   SquareTile(imagePath: 'images/Facebook.png')
                 ],
               ),
-
-          SizedBox(width: getPageWidth(context),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                 Text("Don't have an Account?", style: TextStyle(fontSize: 18),),
-                TextButton(onPressed: (){
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => LoginPage()),
-                  );
-                }, child: const Text('Create an Account',style: TextStyle(fontSize: 18),))
-              ],
-            ),
-          ),
-
-
+              SizedBox(
+                width: getPageWidth(context),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an Account?",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => CreateAccount()),
+                          );
+                        },
+                        child: const Text(
+                          'Create an Account',
+                          style: TextStyle(fontSize: 18),
+                        ))
+                  ],
+                ),
+              ),
             ],
           ),
         ),
       ),
-
     );
   }
 }
