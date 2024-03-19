ba4d96894a21d2aa135e5da5914560e04ef94e3a
Shahiru Edirisinghe <98458540+funky-dev99@users.noreply.github.com>
2024-03-19 21:39:10 +0530

    Map themes Added

--- nextbus_passenger/lib/main.dart
+++ nextbus_passenger/lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:nextbus_passenger/pages/splashScreen.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import 'firebase_options.dart';
 
 Future <void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Permission.locationWhenInUse.isDenied.then((valueOfPermission)
+  {
+    if(valueOfPermission){
+      Permission.locationWhenInUse.request();
+    }
+  });
   runApp(MyApp());
 }
 
--- nextbus_passenger/lib/pages/startTripPage.dart
+++ nextbus_passenger/lib/pages/startTripPage.dart
@@ -1,6 +1,10 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:nextbus_passenger/comman_var.dart';
 
@@ -15,6 +19,36 @@ class _StarTripPageState extends State<StarTripPage> {
   final Completer<GoogleMapController> googleMapCompleterController = Completer<GoogleMapController>();
   GoogleMapController? controllerGoogleMap;
 
+  Position? currentPositionOfUser;
+
+  void updateMapTheme (GoogleMapController controller)
+  {
+    getJsonFileFromThemes('themes/map_night.json').then((value)=>setGoogleMapStyle(value,controller));
+  }
+
+  Future<String> getJsonFileFromThemes (String mapStylePath) async{
+    ByteData byteData = await rootBundle.load(mapStylePath);
+    var list = byteData.buffer.asUint8List(byteData.offsetInBytes,byteData.lengthInBytes);
+    return utf8.decode(list);
+  }
+
+  setGoogleMapStyle(String googleMapStyle, GoogleMapController controller)
+  {
+    controller.setMapStyle(googleMapStyle);
+  }
+
+  getCurrentLiveLocationOfUser() async
+  {
+    Position positionOfUser = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.bestForNavigation);
+    currentPositionOfUser=positionOfUser;
+
+    LatLng positionOfUserInLatLng=LatLng(currentPositionOfUser!.latitude, currentPositionOfUser!.longitude);
+
+    CameraPosition cameraPosition = CameraPosition(target: positionOfUserInLatLng,zoom: 15);
+
+    controllerGoogleMap!.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +60,9 @@ class _StarTripPageState extends State<StarTripPage> {
               initialCameraPosition: googleGooglePlexInitialPosition,
             onMapCreated: (GoogleMapController mapController){
                 controllerGoogleMap=mapController;
+                updateMapTheme(controllerGoogleMap!);
                 googleMapCompleterController.complete(controllerGoogleMap);
+                getCurrentLiveLocationOfUser();
 
             },
 
