454ae629786fdf0d803cdd552efe7b78b8110f55
Shahiru Edirisinghe <98458540+funky-dev99@users.noreply.github.com>
2024-03-23 12:03:52 +0530

    Update startTripPage.dart

--- nextbus_passenger/lib/pages/startTripPage.dart
+++ nextbus_passenger/lib/pages/startTripPage.dart
@@ -8,6 +8,9 @@ import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:nextbus_passenger/comman_var.dart';
 
+import '../colors.dart';
+import 'homePage.dart';
+
 class StarTripPage extends StatefulWidget {
   const StarTripPage({super.key});
 
@@ -54,8 +57,10 @@ class _StarTripPageState extends State<StarTripPage> {
     return Scaffold(
       body: Stack(
         children: [
+
+          //Google Map
           GoogleMap(
-            padding: EdgeInsets.only(top: 40),
+            padding: EdgeInsets.only(top: 50,bottom: 300),
               mapType: MapType.normal,
               myLocationEnabled: true,
               initialCameraPosition: googleGooglePlexInitialPosition,
@@ -67,6 +72,102 @@ class _StarTripPageState extends State<StarTripPage> {
 
             },
 
+          ),
+
+          //drawer button
+          Positioned(
+            top: 60,
+            left: 20,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: const
+                [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 5,
+                    spreadRadius: 0.5,
+                    offset: Offset(0.7, 0.7),
+                  ),
+                ],
+              ),
+              child:  CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 20,
+                child: IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => HomePage()),
+                      );
+                    },
+                    icon: Icon(
+                      Icons.arrow_back_rounded,
+                      color: AppColor.iconColor,
+                    )),
+              ),
+            ),
+          ),
+
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: -50,
+            child: Container(
+              height: 250,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+
+                  ElevatedButton(
+                    onPressed: ()
+                    {
+                      // Navigator.push(context, MaterialPageRoute(builder: (c)=> SearchDestinationPage()));
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        shape: const CircleBorder(),
+                        padding: const EdgeInsets.all(24)
+                    ),
+                    child:  Icon(
+                      Icons.search,
+                      color: AppColor.iconColor,
+                      size: 25,
+                    ),
+                  ),
+
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        shape: const CircleBorder(),
+                        padding: const EdgeInsets.all(24)
+                    ),
+                    child:  Icon(
+                      Icons.home,
+                      color: AppColor.iconColor,
+                      size: 25,
+                    ),
+                  ),
+
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        shape: const CircleBorder(),
+                        padding: const EdgeInsets.all(24)
+                    ),
+                    child:  Icon(
+                      Icons.work,
+                      color: AppColor.iconColor,
+                      size: 25,
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
           ),
         ],
       ),
